555b0a08ab7ca161d51ff5603b6f78968e2b793e
abdulqadirtr <qadirabdul20@yahoo.com>
2022-11-04 16:15:20 +0100

    * project updated

--- app/src/main/java/com/example/dateconversion/MainActivity.kt
+++ app/src/main/java/com/example/dateconversion/MainActivity.kt
@@ -17,17 +17,10 @@ import androidx.annotation.RequiresApi
 import androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY
 
 
+
 class MainActivity : AppCompatActivity() {
     //var date1 = "01-02-2022T07:53:51"
-    var date1 = "2022-02-13T00:00:00"
-    //2022-01-26T00:00:00
-    //2022-01-26
-    var username = "qadirabdul20@yahoo.com"
-    var mailCount = "this format"
-
-    //format string
-    var usernameFormate = "qadirabdul20@yahoo.com"
-    var mailCountFormate = 10
+    var date1 = "2022-02-13T00:10:00"
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,61 +29,21 @@ class MainActivity : AppCompatActivity() {
 
         var myText: TextView = findViewById(R.id.text1)
 
-
-        var formatTextValue: TextView = findViewById(R.id.welcomeFormatText)
-
         val spinner: Spinner = findViewById(R.id.planets_spinner)
 
-        var mytext = getString(R.string.welcome_messages2, usernameFormate, mailCountFormate)
-
-        //for escape character
-
-        val styledText: Spanned = Html.fromHtml(mytext, FROM_HTML_MODE_LEGACY)
-
-        formatTextValue.text = styledText
-
-       // for yesterday and today first we need to formate the date, our date date1 is already in correct format
-        //if not we can format the date as below
-         var formattedDate = date1.formatStringDate(DateFormats.DEFAULT_FORMAT_WITHOUT_TIME.format,DateFormats.TRANSACTION_FILTER_FORMAT.format)
-         var isTodayYesterday = date1.getYesterdayToday(date1, DateFormats.TRANSACTION_FILTER_FORMAT.format)
-
-
+        var formattedDate = date1.formatStringDate(DateFormats.DEFAULT_FORMAT_WITHOUT_TIME.format,DateFormats.DEFAULT_FORMAT.format)
 
         ArrayAdapter.createFromResource(
             this,
-            R.array.planets_array,
+            R.array.format_date,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             // Apply the adapter to the spinner
             spinner.adapter = adapter
 
-      //TODO date part needs for future
-            //to remove dublicate date to show only single date like in messenger
 
-
-      /*      for (claim in customerClaimList) {
-                if (claim.creationDate.equals(customerClaim.creationDate) || claim.equals(customerClaim)) {
-                    isFound = true
-                    break
-                }
-            }
-            if (!isFound) {
-                customerClaimList.add(Claim(customerClaim.allowFeedback, customerClaim.caseId, customerClaim.caseStatus, customerClaim.category, customerClaim.creationDate, formattedDateTime, customerClaim.creationTime, customerClaim.customerNumber, customerClaim.description))
-            }
-            else
-                customerClaimList.add(Claim(customerClaim.allowFeedback, customerClaim.caseId, customerClaim.caseStatus, customerClaim.category, "", formattedDateTime, customerClaim.creationTime, customerClaim.customerNumber, customerClaim.description))
-        }
-        if (customerClaimList.find {
-                it.creationDate == customerClaim.creationDate
-            } != null) {
-            isFound = true
-        }
-
-        customerClaimList.add(Claim(customerClaim.allowFeedback, customerClaim.caseId, customerClaim.caseStatus, customerClaim.category,  when(isFound) {true -> "" false -> customerClaim.creationDate }, formattedDateTime, customerClaim.creationTime, customerClaim.customerNumber, customerClaim.description))
-*/
-
-        spinner.onItemSelectedListener = object : OnItemSelectedListener {
+            spinner.onItemSelectedListener = object : OnItemSelectedListener {
                 override fun onItemSelected(
                     parentView: AdapterView<*>?,
                     selectedItemView: View,
@@ -102,52 +55,83 @@ class MainActivity : AppCompatActivity() {
                         1 -> {
                             var dayMonth = date1
                             dayMonth =
-                                dayMonth.convertDateToDayMonth(DateFormats.DEFAULT_FORMAT_WITHOUT_TIME.format)
+                                dayMonth.convertDefaultWithoutTime(DateFormats.DEFAULT_FORMAT.format)
                                     .toString()
                             myText.text = dayMonth
-                            formatTextValue.text =
-                                getString(R.string.welcome_messages, myText.text, mailCount)
                         }
                         2 -> {
                             var trimhour = date1
                             trimhour =
-                                trimhour.trimHourFromDate(DateFormats.DEPOSIT_FORMAT2.format).toString()
+                                trimhour.convertDateToYMDFullTimeDate(DateFormats.DEFAULT_FORMAT.format).toString()
                             myText.text = trimhour
-                            formatTextValue.text =
-                                getString(R.string.welcome_messages, myText.text, mailCount)
                         }
                         3 -> {
                             var timeAMPM = date1
-                            timeAMPM = timeAMPM.convertDateToAmPm(DateFormats.DEPOSIT_FORMAT2.format)
+                            timeAMPM = timeAMPM.convertDateToYMDFullTimeDate(DateFormats.DEFAULT_FORMAT.format)
                                 .toString()
                             myText.text = timeAMPM
                         }
                         4 -> {
                             var timeAMPM = date1
-                            timeAMPM = timeAMPM.convertDateToDMYWithCommaSeparator(
-                                DateFormats.DEPOSIT_FORMAT2.format,
-                                ","
+                            timeAMPM = timeAMPM.trimTimeFromDateMDY(
+                                DateFormats.DEFAULT_FORMAT.format,
                             ).toString()
                             myText.text = timeAMPM
-                            formatTextValue.text =
-                                getString(R.string.welcome_messages, myText, mailCount)
                         }
+
                         5 -> {
-                            //check if date is today or yesterday
-                            when(isTodayYesterday) {
-                                TODAY -> {
-                                    myText.text = TODAY
-                                }
-                                YESTERDAY -> {
-                                    myText.text = YESTERDAY
-                                }
-                            }
-                            formatTextValue.text =
-                                getString(R.string.welcome_messages, myText.toString(), mailCount)
+                            var timeAMPM = date1
+                            timeAMPM = timeAMPM.convertDateToDMYFullTimeDate(
+                                DateFormats.DEFAULT_FORMAT.format
+                            ).toString()
+                            myText.text = timeAMPM
+                        }
+                        6 -> {
+                            var timeAMPM = date1
+                            timeAMPM = timeAMPM.convertDateToYMD(
+                                DateFormats.DEFAULT_FORMAT.format
+                            ).toString()
+                            myText.text = timeAMPM
+                        }
+
+                        7 -> {
+                            var timeAMPM = date1
+                            timeAMPM = timeAMPM.convertDateToMonthNameYear(
+                                DateFormats.DEFAULT_FORMAT.format
+                            ).toString()
+                            myText.text = timeAMPM
+                        }
+
+                        8 -> {
+                            var timeAMPM = date1
+                            timeAMPM = timeAMPM.convertDateToWeekNameYear(
+                                DateFormats.DEFAULT_FORMAT.format
+                            ).toString()
+                            myText.text = timeAMPM
+                        }
+
+                        9 -> {
+                            var timeAMPM = date1
+                            timeAMPM = timeAMPM.convertDateMonthName(
+                                DateFormats.DEFAULT_FORMAT.format
+                            ).toString()
+                            myText.text = timeAMPM
+                        }
+                        10 -> {
+                            var timeAMPM = date1
+                            timeAMPM = timeAMPM.convertDateCustom(
+                                DateFormats.DEFAULT_FORMAT.format
+                            ).toString()
+                            myText.text = timeAMPM
+                        }
+                        11 -> {
+                            var timeAMPM = date1
+                            timeAMPM = timeAMPM.convertDateToAmPm(
+                                DateFormats.DEFAULT_FORMAT.format
+                            ).toString()
+                            myText.text = timeAMPM
                         }
                     }
-
-
                 }
 
                 override fun onNothingSelected(parentView: AdapterView<*>?) {
@@ -157,8 +141,17 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    companion object {
-        private const val TODAY = "today"
-        private const val YESTERDAY = "yesterday"
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/dateconversion/String.kt
+++ app/src/main/java/com/example/dateconversion/String.kt
@@ -4,72 +4,19 @@ import android.util.Log
 import android.util.Patterns
 import java.text.SimpleDateFormat
 import java.util.*
-
-fun String.times(count: Int): String {
-    val builder = StringBuilder()
-    for (i in 0 until count) {
-        builder.append(this)
-    }
-    return builder.toString()
+fun String.convertDefaultWithoutTime(format: String): String? {
+    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+    val parsedDate = dateFormat.parse(this)
+    val dayMonthFormat = SimpleDateFormat(DateFormats.DEFAULT_FORMAT_WITHOUT_TIME.format, Locale.getDefault())
+    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
 }
 
-fun String.checkLength(): Boolean = length in 8..16
-
-fun String.checkIncludesOneLowercaseLetter(): Boolean = "^(.*[a-z].*)$".toRegex().matches(this)
-
-fun String.checkIncludesOneUppercaseLetter(): Boolean = "^(.*[A-Z].*)$".toRegex().matches(this)
-
-fun String.checkIncludesOneNumber(): Boolean = "^(.*[0-9].*)\$".toRegex().matches(this)
-
-fun String.checkIncludesBirthday(birthDate: String): Boolean {
-    if (length < 4) return false
-    return !contains(birthDate)
+fun String.convertDateToYMDTime(format: String): String? {
+    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+    val parsedDate = dateFormat.parse(this)
+    val dayMonthFormat = SimpleDateFormat(DateFormats.DATE_TIME_YY_MM_FULL_TIME_FORMAT.format, Locale.getDefault())
+    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
 }
-
-fun String.checkSameOrConsecutive(): Boolean {
-    if (length < 4) return false
-    val consecutiveRegex =
-        "^.*(0123|1234|2345|3456|4567|5678|6789|3210|4321|5432|6543|7654|8765|9876|0000|1111|2222|3333|4444|5555|6666|7777|8888|9999).*\$".toRegex()
-    return !consecutiveRegex.matches(this)
-}
-
-/**
- * Basic validation of IBAN with mod-97. If the IBAN is valid, remainder equals 1.
- * for more information: https://en.wikipedia.org/wiki/International_Bank_Account_Number#Algorithms
- */
-fun String?.validateIban(): Boolean {
-    if (this.isNullOrEmpty())
-        return false
-
-    val IBAN_MIN_SIZE = 15
-    val IBAN_MAX_SIZE = 34
-    val IBAN_MAX: Long = 999999999
-    val IBAN_MODULUS: Long = 97
-
-    val trimmed = replace(" ", "")
-    if (trimmed.length < IBAN_MIN_SIZE || trimmed.length > IBAN_MAX_SIZE)
-        return false
-
-    val reformat = trimmed.substring(4) + trimmed.substring(0, 4)
-
-    var total = 0L
-    for (element in reformat) {
-        val charValue = Character.getNumericValue(element)
-        if (charValue < 0 || charValue > 35) {
-            return false
-        }
-        total = (if (charValue > 9) total * 100 else total * 10) + charValue
-        if (total > IBAN_MAX)
-            total %= IBAN_MODULUS
-    }
-
-    return total % IBAN_MODULUS == 1L
-}
-
-fun String.nameValidation() = trim().contains(" ") && length > 4 && !contains("  ")
-
-fun String?.mailValidation() = !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
-
 
 fun String?.formatStringDate(inputFormat : String, outputFormat : String) : String {
     return if (this.isNullOrEmpty()){
@@ -81,39 +28,60 @@ fun String?.formatStringDate(inputFormat : String, outputFormat : String) : Stri
     }
 }
 
-fun String.convertDateToDayMonth(format: String): String? {
+fun String.convertDateToYMDFullTimeDate(format: String): String? {
     val dateFormat = SimpleDateFormat(format, Locale.getDefault())
     val parsedDate = dateFormat.parse(this)
-    val dayMonthFormat = SimpleDateFormat("dd MMM", Locale.getDefault())
+    val dayMonthFormat = SimpleDateFormat(DateFormats.DATE_TIME_YY_MM_FORMAT.format, Locale.getDefault())
     return parsedDate?.let { dayMonthFormat.format(parsedDate) }
 }
 
-fun String.convertDateToDMYWithDotSeperator(format: String): String? {
+
+fun String.trimTimeFromDateMDY(format: String): String? {
     val dateFormat = SimpleDateFormat(format, Locale.getDefault())
     val parsedDate = dateFormat.parse(this)
-    val dayMonthFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
-    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
-}
-
-fun String.convertDateToDMYWithCommaSeparator(inputFormat: String, conversionFormat: String): String? {
-    val dateFormat = SimpleDateFormat(inputFormat, Locale.getDefault())
-    val parsedDate = dateFormat.parse(this)
-    val dayMonthFormat = SimpleDateFormat(conversionFormat, Locale.getDefault())
-    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
-}
-
-fun String.trimHourFromDate(format: String): String? {
-    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
-    val parsedDate = dateFormat.parse(this)
-    val trimmed = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val trimmed = SimpleDateFormat(DateFormats.DATE_MM_DD_YY_FORMAT.format, Locale.getDefault())
     return parsedDate?.let { trimmed.format(parsedDate) }
 }
 
-fun String.trimTimeFromDate(format: String): String? {
+fun String.convertDateToDMYFullTimeDate(format: String): String? {
     val dateFormat = SimpleDateFormat(format, Locale.getDefault())
     val parsedDate = dateFormat.parse(this)
-    val trimmed = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-    return parsedDate?.let { trimmed.format(parsedDate) }
+    val dayMonthFormat = SimpleDateFormat(DateFormats.FULL_DATE_TIME_DD_MM_FORMAT.format, Locale.getDefault())
+    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
+}
+
+fun String.convertDateToYMD(format: String): String? {
+    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+    val parsedDate = dateFormat.parse(this)
+    val dayMonthFormat = SimpleDateFormat(DateFormats.DATE_YY_MM_FORMAT.format, Locale.getDefault())
+    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
+}
+
+fun String.convertDateToMonthNameYear(format: String): String? {
+    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+    val parsedDate = dateFormat.parse(this)
+    val dayMonthFormat = SimpleDateFormat(DateFormats.DATE_MONTH_OF_YEAR_FORMAT.format, Locale.getDefault())
+    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
+}
+
+fun String.convertDateToWeekNameYear(format: String): String? {
+    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+    val parsedDate = dateFormat.parse(this)
+    val dayMonthFormat = SimpleDateFormat(DateFormats.DAY_OF_WEEK_MONTH_FORMAT.format, Locale.getDefault())
+    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
+}
+fun String.convertDateMonthName(format: String): String? {
+    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+    val parsedDate = dateFormat.parse(this)
+    val dayMonthFormat = SimpleDateFormat(DateFormats.DATE_MONTH_FORMAT.format, Locale.getDefault())
+    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
+}
+
+fun String.convertDateCustom(format: String): String? {
+    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+    val parsedDate = dateFormat.parse(this)
+    val dayMonthFormat = SimpleDateFormat(DateFormats.DATE_TIME_FORMAT_CUSTOM_FORMAT.format, Locale.getDefault())
+    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
 }
 
 fun String.convertDateToAmPm(format: String): String? {
@@ -123,52 +91,3 @@ fun String.convertDateToAmPm(format: String): String? {
     return parsedDate?.let { dayMonthFormat.format(parsedDate) }
 }
 
-fun String.convertTime(format: String): String? {
-    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
-    val parsedDate = dateFormat.parse(this)
-    val dayMonthFormat = SimpleDateFormat("hh:mm", Locale.getDefault())
-    return parsedDate?.let { dayMonthFormat.format(parsedDate) }
-}
-
-fun getTodayDate(format: String): String{
-    val date = Calendar.getInstance().time
-    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
-    return dateFormat.format(date)
-}
-
-fun String.groupInFour():String {
-    return replace("....(?!$)".toRegex(),"$0 ")
-}
-
-/**
- * Extension method that converts date for given format
- */
-fun Date.formatToViewTime(customFormat: String = "dd MMMM yyyy"): String {
-    val sdf = SimpleDateFormat(customFormat, Locale.getDefault())
-    Calendar.getInstance().time
-    return sdf.format(this)
-}
-
-//for yesterday today
-fun String?.getYesterdayToday(date: String, format : String): String {
-    try {
-        val formatter = SimpleDateFormat(format)
-        val date = formatter.parse(date)
-        val timeInMilliseconds = date.time
-
-        return when {
-            DateUtils.isToday(timeInMilliseconds) -> {
-                "today"
-            }
-            DateUtils.isToday(timeInMilliseconds+ DateUtils.DAY_IN_MILLIS) -> {
-                "yesterday"
-            }
-            else -> {
-                ""
-            }
-        }
-    } catch (ex: Exception) {
-        ex.message?.let { Log.d("date exception", it) }
-    }
-    return ""
-}
